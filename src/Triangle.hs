ff9275e3d7a5ee75156966cb471337cb13da1a0d
Const_ <kkcnst@gmail.com>
2016-05-14 15:59:11 +0200

    More complex shaders
    color data and uniform variables, simple loop

--- src/Triangle.hs
+++ src/Triangle.hs
@@ -1,19 +1,24 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad
 import qualified Data.Map as M
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.IO.Class (MonadIO)
+import Linear.Matrix
+import Linear.Projection
+import Linear.V3
 import Data.Vector.Storable (Vector)
 import qualified Data.Vector.Storable as V
 import Foreign.Storable (Storable(..))
 import Text.InterpolatedString.Perl6 (q)
-import Control.Monad.Loops (iterateUntil)
+--import Control.Monad.Loops (iterateUntil)
 import Graphics.Caramia
 import SDL
 
 -- Total size of a vector; maybe useful enough to move inside library
 sizeOfV :: forall a. (Storable a) => Vector a -> Int
-sizeOfV vec = fromIntegral $ sizeOf (undefined :: a) * V.length vec
+sizeOfV vec = sizeOf (undefined :: a) * V.length vec
 
 main :: IO ()
 main = do
@@ -27,22 +32,23 @@ main = do
     pl <- newPipelineVF vxsource fgsource M.empty
     vao <- newVAO
     buff <- newBuffer defaultBufferCreation { accessHints = (Static, Draw)
-                                            , size = sizeOfV triangle
+                                            , size = sizeOfV triangle + sizeOfV trianglecol
                                             }
     uploadVector triangle 0 buff
     sourceVertexData buff defaultSourcing { components = 3
                                           , attributeIndex = 0
                                           , sourceType = SFloat
                                           } vao
-    runDraws defaultDrawParams { pipeline = pl } $ do
-      drawR drawCommand { primitiveType = Triangles
-                        , primitivesVAO = vao
-                        , numIndices = 3
-                        , sourceData = Primitives 0
-                        }
-  
-  glSwapWindow w
-  void $ iterateUntil (\e -> eventPayload e == QuitEvent) waitEvent
+
+    uploadVector trianglecol (sizeOfV triangle) buff
+    sourceVertexData buff defaultSourcing { offset = (sizeOfV triangle)
+                                          ,components = 3
+                                          , attributeIndex = 1
+                                          , sourceType = SFloat
+                                          } vao
+    glSwapWindow w
+    drawloop pl vao w
+
   glDeleteContext c
   destroyWindow w
 
@@ -51,27 +57,67 @@ main = do
                               , 1.0, -1.0, 0.0
                               , 0.0, 1.0, 0.0
                               ]
+        trianglecol :: Vector Float
+        trianglecol = V.fromList [1.0, 0.0, 0.0
+                               , 0.0, 1.0, 0.0
+                               , 0.0, 0.0, 1.0
+                               ]
         vxsource =
           [q|
            #version 330 core
 
-           layout(location = 0) in vec3 vpos;
+           uniform mat4 projectionMat;
+           uniform mat4 modelViewMat;
 
+           layout(location = 0) in vec3 vpos;
+           layout(location = 1) in vec3 col;
+           
+           smooth out vec3 ocol;
+            
            void main()
            {
-             gl_Position.xyz = vpos;
-             gl_Position.w = 1.0;
-             gl_Position = gl_Position;
+             ocol = col;
+             gl_Position = projectionMat * modelViewMat * vec4(vpos, 1.0);
            }
           |]
         fgsource =
           [q|
            #version 330 core
-         
-           out vec3 color;
+           
+           smooth in vec3 ocol;
+           out vec4 color;
  
            void main()
            {
-             color = vec3(1, 0, 0);
+             color = vec4(ocol, 1.0);
            }
           |]
+
+
+drawloop :: Pipeline -> VAO -> Window -> IO ()
+drawloop pl vao w = 
+  do
+    events <- pollEvents
+    let noexitEv = (null (filter (\e -> eventPayload e == QuitEvent) events))
+    when noexitEv $ do
+      Graphics.Caramia.clear clearing { clearColor = Just $ rgba 0.4 0.4 0.4 1.0
+                                      } screenFramebuffer
+      runDraws defaultDrawParams { pipeline = pl } $ do
+        mvMloc <- getUniformLocation "modelViewMat" pl
+        pMloc <- getUniformLocation "projectionMat" pl
+        setUniform mvM mvMloc pl
+        setUniform pM pMloc pl
+
+        drawR drawCommand { primitiveType = Triangles
+                          , primitivesVAO = vao
+                          , numIndices = 3
+                          , sourceData = Primitives 0
+                          }
+      runPendingFinalizers
+      glSwapWindow w
+      drawloop pl vao w
+  where mvM :: M44 Float
+        pM :: M44 Float   
+        mvM = transpose ( lookAt (V3 (-10.0) 0.0 (12.0)) (V3 0.0 0.0 0.0) (V3 0.0 0.1 0.0))
+        pM = transpose (perspective (pi/4.0) (4.0/3.0) 0.1 1000.0)
+
