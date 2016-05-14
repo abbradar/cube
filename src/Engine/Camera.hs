e881bec9735ec839afebf4fe4e09d1add1a902b5
Nikolay Amiantov <ab@fmap.me>
2016-05-14 20:32:54 +0300

    Prettify camera

--- src/Engine/Camera.hs
+++ src/Engine/Camera.hs
@@ -5,16 +5,27 @@ import Linear.Projection
 import Linear.V3
 import Data.Default
 
-data Camera = Camera { eye::(V3 Float), center::(V3 Float), up::(V3 Float),
- fov::(Float), ratio::(Float), nplane::(Float), fplane::(Float)} deriving (Show, Eq)
+data Camera = Camera { eye :: V3 Float
+                     , center :: V3 Float
+                     , up :: V3 Float
+                     , fov :: Float
+                     , ratio :: Float
+                     , nplane :: Float
+                     , fplane :: Float
+                     } deriving (Show, Eq)
 
 instance Default Camera where
-  def = Camera { eye = (V3 5.0 0.0 5.0), center = (V3 0.0 0.0 0.0), up = (V3 0.0 1.0 0.0)
-               , fov = (pi/4.0), ratio = (4.0/3.0), nplane = 0.1, fplane = 1000.0
+  def = Camera { eye = (V3 5.0 0.0 5.0)
+               , center = (V3 0.0 0.0 0.0)
+               , up = (V3 0.0 1.0 0.0)
+               , fov = pi/4.0
+               , ratio = 4.0/3.0
+               , nplane = 0.1
+               , fplane = 1000.0
                }
 
 projectionMatrix :: Camera -> (M44 Float) 
-projectionMatrix cam = transpose (perspective (fov cam) (ratio cam) (nplane cam) (fplane cam))
+projectionMatrix (Camera {..}) = transpose $ perspective fov ratio nplane fplane
 
 viewMatrix :: Camera -> (M44 Float) 
-viewMatrix cam = transpose (lookAt (eye cam) (center cam) (up cam))
+viewMatrix (Camera {..}) = transpose $ lookAt eye center up
