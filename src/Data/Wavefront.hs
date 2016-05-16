4c73f197527a82da39486741b1ff4a9b9823be5b
Nikolay Amiantov <ab@fmap.me>
2016-05-16 21:51:42 +0300

    Move OBJ parser to Alex/Happy

--- src/Data/Wavefront.hs
+++ src/Data/Wavefront.hs
@@ -1,55 +1,13 @@
 module Data.Wavefront
-       ( Wavefront(..)
+       ( WFValue(..)
        , parseOBJ
        , WFModel(..)
        , extractModel
        ) where
 
-import Control.Monad
-import Control.Applicative
-import Data.Word (Word16)
 import Data.Default
-import Data.ByteString (ByteString)
-import Linear.V3 (V3(..))
-import Data.Attoparsec.ByteString.Char8
 
-type F3 = V3 Float
-type I3 = V3 Word16
-
-data Wavefront = WFVertex F3
-               | WFNormal F3
-               | WFFace I3
-               deriving (Show, Eq)
-
-float :: Parser Float
-float = fromRational <$> toRational <$> double
-
-objSpace :: Parser ()
-objSpace = skipWhile (== ' ')
-
-objFloat :: Parser Float
-objFloat = objSpace *> float
-
-objWord :: Parser Word16
-objWord = objSpace *> decimal
-
-pointF3 :: Parser F3
-pointF3 = V3 <$> objFloat <*> objFloat <*> objFloat
-
-pointI3 :: Parser I3
-pointI3 = V3 <$> objWord <*> objWord <*> objWord
-
-skipComments :: Parser ()
-skipComments = skipSpace <* optional (char '#' >> skipWhile (/= '\n') >> skipComments)
-
-prefix :: ByteString -> Parser ()
-prefix s = void $ string s *> char ' '
-
-parseOBJ :: Parser [Wavefront]
-parseOBJ = many (skipComments *> line) <* skipSpace <* endOfInput
-  where line =     WFVertex <$> (prefix "v" *> pointF3)
-               <|> WFNormal <$> (prefix "vn" *> pointF3)
-               <|> WFFace <$> (prefix "f" *> pointI3)
+import Data.Wavefront.Parser
 
 data WFModel = WFModel { wfNormals :: [F3]
                        , wfVertices :: [F3]
@@ -64,7 +22,7 @@ instance Default WFModel where
                 , wfIndices = []
                 }
 
-extractModel :: [Wavefront] -> WFModel
+extractModel :: [WFValue] -> WFModel
 extractModel [] = def
 extractModel (h:t) = case h of
   WFVertex a -> m { wfVertices = a : wfVertices }
