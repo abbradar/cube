f44925dc4a46ece7e41f55e1294bdc05a6285820
Nikolay Amiantov <ab@fmap.me>
2017-09-02 15:48:38 +0300

    Update again

--- default.nix
+++ default.nix
@@ -8,13 +8,18 @@ mkDerivation {
   pname = "cube";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
-  executableHaskellDepends = [
+  libraryHaskellDepends = [
     attoparsec base bytestring c-storable-deriving caramia containers
-    criterion data-default data-default-class interpolatedstring-perl6
-    JuicyPixels lens linear scientific sdl2 storable-tuple text
-    transformers uuid-types vector
+    data-default data-default-class interpolatedstring-perl6
+    JuicyPixels lens linear scientific sdl2 storable-tuple transformers
+    uuid-types vector
   ];
+  executableHaskellDepends = [
+    base caramia containers data-default data-default-class lens linear
+    sdl2 text
+  ];
+  benchmarkHaskellDepends = [ base bytestring criterion ];
   license = stdenv.lib.licenses.bsd3;
 }
